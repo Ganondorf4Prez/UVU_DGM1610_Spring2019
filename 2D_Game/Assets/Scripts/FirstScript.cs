485b2f63e926058c840a0fb812609a1c33631285
Zach Moss <zachmoss27@gmail.com>
2019-01-14 18:29:57 -0700

    Added "FirstScript" with 'waterBottle' object.

---
+++ 2D_Game/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour {
+
+    public int waterBottle = 60;
+
+	void Start () {
+        print(waterBottle);
+        Debug.Log(waterBottle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
