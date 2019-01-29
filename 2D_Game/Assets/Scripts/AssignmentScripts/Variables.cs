c3d10bd6cfc85b810178debf44ee580e21d90147
Zach Moss <zachmoss27@gmail.com>
2019-01-28 17:24:25 -0700

    Made various script changes

---
+++ 2D_Game/Assets/Scripts/AssignmentScripts/Variables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+    public int number;
+    public float speed;
+    public string name;
+
+	void Start () {
+        // This is the syntax for a comment.
+
+        number = 10;
+        speed = 0.94f;
+        name = "Zach";
+
+		/* 
+         
+         check
+         out
+         this
+         info
+     
+          */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(number, 0, 0);
+	}
+}
