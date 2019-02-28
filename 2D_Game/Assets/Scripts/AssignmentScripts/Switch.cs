543715b73a332c96e3db179b09b1f8b5b54903f4
Zach Moss <zachmoss27@gmail.com>
2019-02-27 17:17:48 -0700

    Switch Statement
    In-Class switch Statements

---
+++ 2D_Game/Assets/Scripts/AssignmentScripts/Switch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public string suspect;
+    public string weapon;
+    public string room;
+
+
+	void Start () {
+        MurderMystery(suspect);
+	}
+	void Update () {
+	}
+    void MurderMystery(string person)
+    {
+        switch (person)
+        {
+            case "Mr. Ketchup":
+            case "Colonel Mustard":
+                print("I was in the billiard room smoking a cigar and playing...cut throat...");
+                break;
+            case "Ms. Frysauce":
+                print("I was talking on the rotory phone with my mother, Mrs. Mayo");
+                break;
+            case "Mrs. Mayo":
+                print("I was in the kitchen cleaning up the dishes with the scrubber");
+                break;
+            default:
+                print("I am not familiar with " + person + "!");
+                break;
+        }
+    }
+
+}
+
