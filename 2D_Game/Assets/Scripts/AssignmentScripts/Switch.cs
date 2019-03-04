5ec500a2776a7556ae48b3e0eddfa985f96c1af8
Zach Moss <zachmoss27@gmail.com>
2019-03-04 16:23:12 -0700

    Uploaded Switches HW
    Added a few homework switches to the original switch statement script

--- 2D_Game/Assets/Scripts/AssignmentScripts/Switch.cs
+++ 2D_Game/Assets/Scripts/AssignmentScripts/Switch.cs
@@ -33,6 +33,64 @@ public class Switch : MonoBehaviour {
                 break;
         }
     }
+    void StopLight(string light)
+    {
+        switch (light)
+        {
+            case "Red":
+            case "Flashing Red":
+                print("The light designates a stop");
+                break;
+            case "Yellow":
+                print("The light designates slowing down");
+                break;
+            case "Green":
+                print("The light designates passing freely");
+                break;
+            default:
+                print("The light color, " + light + "is unknown");
+                break;
 
-}
+        }
+    }
+    void Weather(string outside)
+    {
+        switch (outside)
+        {
+            case "Snowy":
+                print("Enjoy the weather outside!");
+                break;
+            case "Windy":
+                print("May need a jumper!");
+                break;
+            case "Sunny":
+                print("Avoid the cancer out there...");
+                break;
+            case "Cloudy":
+                print("Nice on the eyes today");
+                break;
+            default:
+                print("The weather outside is " + outside);
+                break;
+
+        }
+    }
+    void Switches(string swap)
+    {
+        switch (swap)
+        {
+            case "Swich":
+            case "Switched":
+            case "Switches":
+            case "Switching":
+                print("Switched");
+                break;
+            default:
+                print("Left unswitched");
+                break;
+            
+        }
+    }
+}   
+
 
