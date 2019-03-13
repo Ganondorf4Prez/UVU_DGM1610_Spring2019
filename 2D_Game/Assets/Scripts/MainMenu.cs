a82a766093f949d8836276c79dcfcfcb45b02858
Zach Moss <zachmoss27@gmail.com>
2019-03-13 17:17:33 -0600

    Main Menu
    Added MainMenu scene, as well as 2 accompanying UI buttons

---
+++ 2D_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    //Indexed Level to load
+    public int levelToLoad;
+
+    //This function loads the level stored in the levelToLoad variable
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    //This function quits the game after the button is pressed
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+
+}
