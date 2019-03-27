496876794a3d3cbdc9486db658efae7b47a9aa74
Zach Moss <zachmoss27@gmail.com>
2019-03-27 17:19:15 -0600

    For Each / Level Manager
    Added ForEach and DoWhile loops into the Loop script; began work on the camera follow / respawn scripts

--- 2D_Game/Assets/Scripts/AssignmentScripts/Loops.cs
+++ 2D_Game/Assets/Scripts/AssignmentScripts/Loops.cs
@@ -14,6 +14,7 @@ public class Loops : MonoBehaviour {
             print(i + " bottles of Apple-Beer on the wall");
             
         }
+        //Homework Loops
         for(int x = 1; x < 11; x++)
         {
             print("Let's count up to 10");
@@ -30,13 +31,29 @@ public class Loops : MonoBehaviour {
             bottles++;
         }
         count = 0;
+        //Homework Loop
         while(count != 10)
         {
             
             count++;
         }
+        //Do While
+        bool shouldContinue = false;
 
+        do
+        {
+            print("Hello World");
+        } while (shouldContinue == true);
+        //For Each Loop
+        string[] strings = new string[3];
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
 
+        foreach(string item in strings)
+        {
+            print(item);
+        }
 	}
 
 }
