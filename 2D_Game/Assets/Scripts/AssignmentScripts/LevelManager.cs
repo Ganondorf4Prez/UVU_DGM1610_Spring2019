b7b387d1b4e20d02fdbff6904dd88048c10cd768
Zach Moss <zachmoss27@gmail.com>
2019-04-01 17:16:18 -0600

    Killscript, LevelManager
    Added scripts for killplayer, levelmanager; added these too Baddy and LevelManager, respectively.

--- 2D_Game/Assets/Scripts/AssignmentScripts/LevelManager.cs
+++ 2D_Game/Assets/Scripts/AssignmentScripts/LevelManager.cs
@@ -33,6 +33,45 @@ public class LevelManager : MonoBehaviour {
         StartCoroutine("RespawnPlayerCo");
     }
 	
+    //IEnumerator is the CoRoutine
+    public IEnumerator RespawnPlayerCo()
+    {
+        //Generate Death Particle
+        Instantiate(deathParticle, pcRigid.transform.position, pcRigid.transform.rotation);
+
+        //Hide PC
+        player.SetActive(false);
+        pcRigid.GetComponent<Renderer>().enabled = false;
+
+        //GravityReset
+        gravityStore = pcRigid.GetComponent<Rigidbody2D>().gravityScale;
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        pcRigid.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        //Point Penalty
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+
+        //Debug Message
+        Debug.Log("PC Respawn");
+
+        //Respawn Delay
+        yield return new WaitForSeconds(respawnDelay);
+
+        //Gravity Restore
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+
+        //Match pcRigids transform position
+        pcRigid.transform.position = currentCheckPoint.transform.position;
+
+        //Show pcRigid
+        player.SetActive(true);
+        pcRigid.GetComponent<Renderer>().enabled = true;
+
+        //Spawn pcRigid
+        Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
