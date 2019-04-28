71bb1a23c564aba2a77ebe8d279e3f7e57178175
Ganon'sTower <zachmoss27@gmail.com>
2019-04-27 18:12:23 -0600

    PowerUp
    Created working powerup script and additional artwork to accompany it (SuperTank). Added another animated rock that can be shot to destroy

---
+++ 2D_Game/Assets/Scripts/PowerUP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP : MonoBehaviour {
+
+    public float multiplier = 4.0f;
+    public float duration;
+    public GameObject pickUpEffect;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            StartCoroutine(PickUp(other));
+        }
+    }
+
+    IEnumerator PickUp(Collider2D player)
+    {
+        Instantiate(pickUpEffect, transform.position, transform.rotation);
+
+        UserCtrl speedup = player.GetComponent<UserCtrl>();
+        speedup.userSpeed *= multiplier;
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        speedup.userSpeed /= multiplier;
+
+
+
+        Destroy(gameObject);
+    }
+
+}
